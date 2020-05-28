1dcc8fb0bf0008761346673b302ed3a9007705f1
lmh <5115979+lmh5388615@user.noreply.gitee.com>
2020-05-28 18:22:54 +0800

    !20 主域部分实现排行榜的功能，但是现在分数还是生成的随机数，需要后续分数生成 * 主域部分的排行榜实现

--- assets/script/changeScene.js
+++ assets/script/changeScene.js
@@ -14,11 +14,29 @@ cc.Class({
         // console.log(G)
         // 加载 SpriteFrame
         var self = this;
-        // console.log(G.userInfo.avatarUrl)这个变量存储的是微信头像的url，但我加载不到组件上。。。
+        console.log(G.userInfo.avatarUrl)
+        //这个变量存储的是微信头像的url，但我加载不到组件上。。。
         //这里想把用户的微信头像加载到主页上，但是加载不上
-        // cc.loader.loadRes({url:G.userInfo.avatarUrl, type: 'png'}, cc.SpriteFrame, function (err, data) {
+        console.log(self.node.children[2].getComponent(cc.Sprite))
+        // cc.loader.load({url:G.userInfo.avatarUrl, type: 'png'}, cc.SpriteFrame, function (err, data) {
         //     self.node.children[2].getComponent(cc.Sprite).spriteFrame = data;
+        //     if(err) {
+        //         console.error(err)
+        //     }
         // });
+
+        cc.loader.load({
+            url: G.userInfo.avatarUrl,
+            type: "png",
+        }, (err, texture) => {
+            if (err) {
+                console.error(err);
+            }
+            let userIcon = self.node.getChildByName('headimage')
+            if (userIcon) {
+                userIcon.getComponent(cc.Sprite).spriteFrame = new cc.SpriteFrame(texture);
+            }
+        })
     },
 
     start() {
